2dcee3e3dd60fc457ea1b20ee32fa9516782a6ea
Ali Raza <razamughaln@gmail.com>
2022-04-21 16:48:24 +0500

    package.json updated

--- src/routes/adminRoutes.jsx
+++ src/routes/adminRoutes.jsx
@@ -1,16 +1,16 @@
-import Dashboard from '@material-ui/icons/Dashboard';
-import List from '@material-ui/icons/ListAlt';
-import DashboardPage from '../views/Dashboard/Dashboard';
-import ManagerRequests from '../views/ManagerRequests/ManagerRequests';
-import Bookings from '../views/Bookings/Bookings';
-import VenueManager from '../views/VenueManager/VenueManager';
-import Payout from '../views/Payouts/Payout';
+import Dashboard from "@material-ui/icons/Dashboard";
+import List from "@material-ui/icons/ListAlt";
+import DashboardPage from "../views/Dashboard/Dashboard";
+import ManagerRequests from "../views/ManagerRequests/ManagerRequests";
+import Bookings from "../views/Bookings/Bookings";
+import VenueManager from "../views/VenueManager/VenueManager";
+import Payout from "../views/Payouts/Payout";
 import PendingPayouts from "../views/PendingPayouts/PendingPayouts";
 import BookingHistory from "../views/BookingHistory/BookingHistory";
-import { HowToReg, HowToRegSharp, MonetizationOnSharp, VerifiedUser } from '@material-ui/icons'
-import AdminAllVenues from '../views/AdminAllVenues/AdminAllVenues'
-import AdminTransactions from '../views/AdminTransactions/AdminTransactions'
-
+import MonetizationOnSharp from "@material-ui/icons/MonetizationOnSharp";
+import HowToRegSharp from "@material-ui/icons/HowToRegSharp";
+import AdminAllVenues from "../views/AdminAllVenues/AdminAllVenues";
+import AdminTransactions from "../views/AdminTransactions/AdminTransactions";
 
 const adminRoutes = [
   {
@@ -18,21 +18,21 @@ const adminRoutes = [
     sidebarName: "Dashboard",
     navbarName: "Material Dashboard",
     icon: Dashboard,
-    component: DashboardPage
+    component: DashboardPage,
   },
   {
     path: "/pendingPayouts",
     sidebarName: "Pending Payouts",
     navbarName: "Pending Payouts",
     icon: MonetizationOnSharp,
-    component: PendingPayouts
+    component: PendingPayouts,
   },
   {
     path: "/managerRequests",
     sidebarName: "Manager Requests",
     navbarName: "Manager Requests",
     icon: HowToRegSharp,
-    component: ManagerRequests
+    component: ManagerRequests,
   },
   // {
   //   path: "/bookingsHistory",
@@ -46,24 +46,24 @@ const adminRoutes = [
     sidebarName: "Booking History",
     navbarName: "Booking History",
     icon: List,
-    component: BookingHistory
+    component: BookingHistory,
   },
   {
     path: "/AdminAllVenues",
     sidebarName: "All Venues",
     navbarName: "All Venues",
     icon: List,
-    component: AdminAllVenues
+    component: AdminAllVenues,
   },
   {
-      path: "/purchases",
-      sidebarName: "Purchases",
-      navbarName: "Purchases",
-      icon: MonetizationOnSharp,
-      component: AdminTransactions
+    path: "/purchases",
+    sidebarName: "Purchases",
+    navbarName: "Purchases",
+    icon: MonetizationOnSharp,
+    component: AdminTransactions,
   },
- 
-  { redirect: true, path: "/", to: "/dashboard", navbarName: "Redirect" }
+
+  { redirect: true, path: "/", to: "/dashboard", navbarName: "Redirect" },
 ];
 
 export default adminRoutes;
