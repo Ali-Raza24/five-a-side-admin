786e06106e02dbd1d17978b4585676b5d6fb9c5e
Ali-Raza24 <89077002+Ali-Raza24@users.noreply.github.com>
2022-04-21 15:33:53 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,3 @@
-// const ROOT_URL = "http://localhost:8000/api/";
-// const REDIRECT = "/login";
-// export default {
-//   ROOT_URL,
-//   REDIRECT,
-// };
-
 const ROOT_URL = "http://63.33.237.96/api/";
 const REDIRECT = "/login";
 export default {
